a7cc4830b41337c681a77c160e8f0436ec74f2fc
Guilherme Estrela Lambert de Brito <guilhermestrela@gmail.com>
2023-09-26 23:34:36 -0300

    salvando foto no local storage

--- js/camera.js
+++ js/camera.js
@@ -1,6 +1,12 @@
 const botaoiniciarCamera = document.querySelector('[data-video-botao]');
 const campoCamera = document.querySelector('[data-camera]');
 const video = document.querySelector('[data-video]');
+const botaoTirafoto = document.querySelector('[data-tirar-foto]');
+const canvas = document.querySelector('[data-video-canvas]');
+const mensagem = document.querySelector('[data-mensagem]');
+const botaoEviarFoto = document.querySelector('[data-enviar]');
+
+let imagemURL = '';
 
 botaoiniciarCamera.addEventListener('click', async function(){
     const iniciarVideo = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
@@ -10,3 +16,23 @@ botaoiniciarCamera.addEventListener('click', async function(){
 
     video.srcObject = iniciarVideo;
 });
+
+botaoTirafoto.addEventListener('click', function(){
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.Width, canvas.Height);
+
+    imagemURL = canvas.toDataURL('image/jpeg');
+
+    campoCamera.style.display = 'none';
+    mensagem.style.display = 'block';
+})
+
+botaoEviarFoto.addEventListener('click', () =>{
+    const receberDadosExistentes = localStorage.getItem('cadastro');
+    const converteRetorno = JSON.parse(receberDadosExistentes);
+
+    converteRetorno.imagem = imagemURL;
+
+    localStorage.setItem('cadastro', JSON.stringify(converteRetorno));
+
+    window.location.href = './abrir-conta-form-3.html';
+})
